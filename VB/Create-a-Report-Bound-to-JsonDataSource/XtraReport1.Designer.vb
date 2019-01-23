3728245da10ed4de1bee1095be6cf87662b05c11
DevExpressExampleBot <fakeEmail>
2019-01-23 15:04:52 +0300

    Source auto update [skip ci]

---
+++ VB/Create-a-Report-Bound-to-JsonDataSource/XtraReport1.Designer.vb
@@ -0,0 +1,58 @@
+﻿Namespace Create_a_Report_Bound_to_JsonDataSource
+    Partial Public Class XtraReport1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+            Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+            Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+            CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' TopMargin
+            ' 
+            Me.TopMargin.Name = "TopMargin"
+            ' 
+            ' BottomMargin
+            ' 
+            Me.BottomMargin.Name = "BottomMargin"
+            ' 
+            ' Detail
+            ' 
+            Me.Detail.Name = "Detail"
+            ' 
+            ' XtraReport1
+            ' 
+            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.TopMargin, Me.BottomMargin, Me.Detail})
+            Me.Font = New System.Drawing.Font("Arial", 9.75F)
+            Me.Version = "18.2"
+            CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+        End Sub
+
+        #End Region
+
+        Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+        Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+        Private Detail As DevExpress.XtraReports.UI.DetailBand
+    End Class
+End Namespace
---
+++ VB/Create-a-Report-Bound-to-JsonDataSource/XtraReport1.vb
@@ -0,0 +1,16 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Collections
+Imports System.ComponentModel
+Imports DevExpress.XtraReports.UI
+
+Namespace Create_a_Report_Bound_to_JsonDataSource
+    Partial Public Class XtraReport1
+        Inherits DevExpress.XtraReports.UI.XtraReport
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+    End Class
+End Namespace
