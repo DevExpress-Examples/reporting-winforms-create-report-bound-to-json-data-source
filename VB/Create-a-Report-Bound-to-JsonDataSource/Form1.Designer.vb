7cfb007ad547be3aded0d39b9052ec250399b72f
DevExpressExampleBot <fakeEmail>
2024-03-12 17:52:11 +0400

    Source auto update [skip ci]

--- VB/Create-a-Report-Bound-to-JsonDataSource/Form1.Designer.vb
+++ VB/Create-a-Report-Bound-to-JsonDataSource/Form1.Designer.vb
@@ -1,44 +1,42 @@
-﻿Namespace Create_a_Report_Bound_to_JsonDataSource
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace Create_a_Report_Bound_to_JsonDataSource
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.SuspendLayout()
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(284, 261)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'INSTANT VB NOTE: The following InitializeComponent event wireup was converted to a 'Handles' clause:
-'ORIGINAL LINE: this.Load += new System.EventHandler(this.Form1_Load);
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
-	End Class
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.SuspendLayout()
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(284, 261)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            Me.ResumeLayout(False)
+        End Sub
+#End Region
+    End Class
 End Namespace
-
--- VB/Create-a-Report-Bound-to-JsonDataSource/XtraReport1.Designer.vb
+++ VB/Create-a-Report-Bound-to-JsonDataSource/XtraReport1.Designer.vb
@@ -1,58 +1,60 @@
-﻿Namespace Create_a_Report_Bound_to_JsonDataSource
-	Partial Public Class XtraReport1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace Create_a_Report_Bound_to_JsonDataSource
 
-		''' <summary> 
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class XtraReport1
 
-		#Region "Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
-			Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
-			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
-			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
-			' 
-			' TopMargin
-			' 
-			Me.TopMargin.Name = "TopMargin"
-			' 
-			' BottomMargin
-			' 
-			Me.BottomMargin.Name = "BottomMargin"
-			' 
-			' Detail
-			' 
-			Me.Detail.Name = "Detail"
-			' 
-			' XtraReport1
-			' 
-			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.TopMargin, Me.BottomMargin, Me.Detail})
-			Me.Font = New System.Drawing.Font("Arial", 9.75F)
-			Me.Version = "18.2"
-			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.TopMargin = New DevExpress.XtraReports.UI.TopMarginBand()
+            Me.BottomMargin = New DevExpress.XtraReports.UI.BottomMarginBand()
+            Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+            CType((Me), System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' TopMargin
+            ' 
+            Me.TopMargin.Name = "TopMargin"
+            ' 
+            ' BottomMargin
+            ' 
+            Me.BottomMargin.Name = "BottomMargin"
+            ' 
+            ' Detail
+            ' 
+            Me.Detail.Name = "Detail"
+            ' 
+            ' XtraReport1
+            ' 
+            Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() {Me.TopMargin, Me.BottomMargin, Me.Detail})
+            Me.Font = New System.Drawing.Font("Arial", 9.75F)
+            Me.Version = "18.2"
+            CType((Me), System.ComponentModel.ISupportInitialize).EndInit()
+        End Sub
 
-		Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
-		Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
-		Private Detail As DevExpress.XtraReports.UI.DetailBand
-	End Class
+#End Region
+        Private TopMargin As DevExpress.XtraReports.UI.TopMarginBand
+
+        Private BottomMargin As DevExpress.XtraReports.UI.BottomMarginBand
+
+        Private Detail As DevExpress.XtraReports.UI.DetailBand
+    End Class
 End Namespace
